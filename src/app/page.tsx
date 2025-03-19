1418abf7d7f4da317284fc0a9c63cb8f77e2e232
luiseduardofrias27 <luiseduardofrias27@gmail.com>
2025-03-19 01:14:12 -0400

    responsive home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
 	const translate = useTranslate();
 	return (
 		<Page>
-			<div className="col-start-1 col-end-2 sm:row-start-1 ms:row-end-2 row-start-2 row-end-3 h-full flex flex-col justify-between gap-[20px] ">
+			<div className="col-start-1 col-end-2 md:row-start-1 ms:row-end-2 row-start-2 row-end-3 h-full flex flex-col justify-between gap-[20px] ">
 				<Presentation />
 				<Menu />
 			</div>
-			<div className="sm:col-start-2 sm:col-end-3 col-start-1 col-end-2 row-start-1 row-end-2 grid-area-right h-full">
+			<div className="md:col-start-2 md:col-end-3 col-start-1 col-end-2 row-start-1 row-end-2 grid-area-right h-full">
 				<Profile />
 			</div>
 		</Page>
