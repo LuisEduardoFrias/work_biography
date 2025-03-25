03a4a66aefa0f4d9c94f8461b1e6457ab77fe88a
luiseduardofrias27 <luiseduardofrias27@gmail.com>
2025-03-25 12:17:35 -0400

    I fixed the orthographic

--- src/components/skills/back_end.tsx
+++ src/components/skills/back_end.tsx
@@ -24,7 +24,7 @@ export function Draw({ skill, index, onclick }: TDrawProps) {
 	return (
 		<div key={index} style={Styles} onClick={onclick} >
 			<div className="border border-theme-1 bg-[#a7a7a78b] group transition-shadow hover:scale-110 hover:shadow-[2px_2px_3px_5px_var(--theme-6)] shadow-[2px_2px_3px_0_var(--theme-4)] p-2 flex flex-col justify-center gap-1 items-center rounded-2xl">
-				{skill.href && <Image src={skill.href} className="group-hover:rotate-45 transition-transform min-w-[100px] min-h-fit" width={70} height={70} alt={skill.alt} />}
+				{skill.href && <Image src={skill.href} className="group-hover:rotate-45 transition-transform max-w-[100px] h-fit" width={100} height={100} alt={skill.alt} />}
 				{skill.name && <span>{skill.name}</span>}
 			</div>
 			<div></div>
@@ -36,7 +36,7 @@ export default function BackEnd() {
 	return (
 		<div className="w-full px-5 space-y-10">
 			<h2>Back End</h2>
-			<div className="flex flex-wrap gap-12">
+			<div className="flex flex-wrap gap-10">
 				{
 					Skills.backend.map((e: Sk, index: number) => <Draw key={index} onclick={() => { }} skill={e} index={index} />)
 				}
