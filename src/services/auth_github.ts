1b6ab320fcf2a040126159ef98c98d3bbaab5e42
luis eduardo <luiseduardofrias27@gmail.com>
2025-03-31 09:30:18 -0400

    refector auth_github

--- src/services/auth_github.ts
+++ src/services/auth_github.ts
@@ -14,22 +14,31 @@ type TypeRepository = {
 	gifUrls: string[],
 }
 
-export async function getUserName() {
+async function getReposSortedByCreation(username: string): Promise<TypeRepository[]> {
 	try {
-		const { data } = await octokit.rest.users.getAuthenticated();
-		return data;
-	} catch (error) {
-		console.log("errr:, %s", error);
-	}
-}
+		let page = 1;
+		let allRepos: TypeRepository[] = [];
+		let hasMore = true;
 
-async function getRepos(username: string) {
-	try {
-		const response = await octokit.request(`GET /users/${username}/repos`, {
-			username: process.env.NEXT_PUBLIC_GITHUB_USER_NAME,
-			type: 'public',
-		});
-		return response.data;
+		while (hasMore) {
+			const response = await octokit.request(`GET /users/${username}/repos`, {
+				username: process.env.NEXT_PUBLIC_GITHUB_USER_NAME,
+				per_page: 100,
+				page: page,
+				sort: 'created',
+				direction: 'desc',
+			});
+
+			if (response.data.length > 0) {
+				allRepos = allRepos.concat(response.data as TypeRepository[]);
+				page++;
+			} else {
+				hasMore = false;
+			}
+		}
+
+
+		return allRepos;
 	} catch (error) {
 		console.error("Error al obtener repositorios:", error);
 		return [];
@@ -38,42 +47,40 @@ async function getRepos(username: string) {
 
 export async function getRepoInfoWithProfile() {
 	const username = process.env.NEXT_PUBLIC_GITHUB_USER_NAME;
-	const file = process.env.NEXT_PUBLIC_GITHUB_REPO_FILE;
 
 	if (!username) {
 		const error = new Error("Se requiere la variable en entorno user_name");
 		throw error;
 	}
 
-	const repos = await getRepos(username);
+	const repos = await getReposSortedByCreation(username);
 	const reposWithProfile: TypeRepository[] = [];
 
 	for (const repo of repos) {
 		try {
-			await octokit.request(`GET /repos/${username}/${repo.name}/contents/${file}`);
+			if (repo.topics.includes('addedtoportfolio')) {
+				const gifFiles = await getFilesInFolder(username, repo.name, "gifs");
+				const gifUrls = getGifUrls(gifFiles);
 
-			const gifFiles = await getFilesInFolder(username, repo.name, "gifs");
-			const gifUrls = getGifUrls(gifFiles);
-
-			reposWithProfile.push({
-				name: repo.name,
-				description: repo.description,
-				created_at: repo.created_at,
-				updated_at: repo.updated_at,
-				pushed_at: repo.pushed_at,
-				topics: repo.topics,
-				language: repo.language,
-				html_url: repo.html_url,
-				gifUrls,
-			});
+				reposWithProfile.push({
+					name: repo.name,
+					description: repo.description,
+					created_at: repo.created_at,
+					updated_at: repo.updated_at,
+					pushed_at: repo.pushed_at,
+					topics: repo.topics.filter(item => item !== 'addedtoportfolio'),
+					language: repo.language,
+					html_url: repo.html_url,
+					gifUrls,
+				});
+			}
 		} catch (error) {
 			if (!error)
 				console.log(error)
-			//console.log(`El repositorio '${repo.name}'' no tiene ${file} o hubo un error.`);
 		}
 	}
 
-	return sortByCreationDate(reposWithProfile);
+	return reposWithProfile;
 }
 
 async function getFilesInFolder(username: string, repoName: string, folderPath: string): Promise<TypeFile[]> {
@@ -82,12 +89,13 @@ async function getFilesInFolder(username: string, repoName: string, folderPath: 
 			`GET /repos/${username}/${repoName}/contents/${folderPath}`
 		);
 
-		// Filtra solo los archivos (no carpetas)
 		const files: TypeFile[] = response.data.filter((item: { type: string }) => item.type === "file");
 
 		return files;
 	} catch (error) {
-		console.error(`Error al obtener archivos en ${folderPath}:`, error);
+		if (!error) {
+			console.error(`Error al obtener archivos en ${folderPath}:`, error);
+		}
 		return [];
 	}
 }
@@ -99,26 +107,6 @@ function getGifUrls(files: TypeFile[]) {
 		.filter((file: TypeFile) => file.name.toLowerCase().endsWith(".gif"))
 		.map((file) => file.download_url);
 }
-
-
-
-function sortByCreationDate(arrayOfObjects: TypeRepository[]): TypeRepository[] {
-	if (!Array.isArray(arrayOfObjects)) {
-		console.log("Invalid input. An array was expected.");
-	}
-
-	return arrayOfObjects.slice().sort((a, b) => {
-		const dateA = new Date(a.created_at).getTime();
-		const dateB = new Date(b.created_at).getTime();
-		return dateA - dateB;
-	}).reverse();
-}
-
-
-
-
-
-
 
 
 
